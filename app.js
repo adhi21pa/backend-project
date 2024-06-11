04effa12f605642991c9095222f217402fd36aa1
adhi21pa <adithyapa21@gmail.com>
2024-06-11 20:51:41 +0530

    complete

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const {studentmodel}=require("./models/student")
+
 const app = express()
 app.use(cors())
-app.get("/",(req,res)=>{
-    res.send("hello")
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://adithyapa2003:adithya66@cluster0.g5hvjxm.mongodb.net/studentdb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/add",(req,res)=>{
+   let input = req.body
+   let student=new studentmodel(input)
+   student.save()
+   console.log(student)
+   res.json({"status":"success"})
 })
-app.get("/contact",(req,res)=>{
-    res.send("Welcome to my contact page")
-})
-app.listen(8080,()=>{
-    console.log("server started")
-})
+app.get("/view",(req,res) => {
+    studentmodel.find().then(
+
+    (data)=>{
+        res.json(data)
+    }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
+            
+        })
+
+
+app.listen(8081)
+console.log("server started")
